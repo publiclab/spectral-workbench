3a00e57b6a3240948ca12c7d440ed31fa8378a5a
jywarren <jeff@unterbahn.com>
2010-11-22 13:59:25 -0300

    validations, tweaks

--- app/models/spectrum.rb
+++ app/models/spectrum.rb
@@ -4,6 +4,11 @@ class Spectrum < ActiveRecord::Base
 	validates_presence_of :author, :on => :create, :message => "can't be blank"
 	validates_presence_of :photo, :on => :create, :message => "can't be blank"
 	# validates_presence_of :data, :on => :create, :message => "can't be blank"
+
+	validates_format_of     :name,
+                            	:with => /[a-zA-Z0-9_-]/,  
+                            	:message => " must not include spaces and must be alphanumeric, as it'll be used in the URL of your spectrum, like: http://site.org/spectra/your-spectrum-name. You may use dashes and underscores.",
+                            	:on => :create
 	
 	# Paperclip
 	has_attached_file :photo,
