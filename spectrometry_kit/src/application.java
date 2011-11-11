b71603711585822c3e6f3e8421847b0739bab005
Jeffrey Warren <jeff@unterbahn.com>
2011-11-10 20:16:00 -0500

    saving function in server class, commandkey for saving (not tested) upload to server with serverURL

--- spectrometry_kit/src/application.java
+++ spectrometry_kit/src/application.java
@@ -37,7 +37,11 @@ System system;
 Video video;
 //= require <models/filter>
 Filter filter;
+//= require <models/server>
+Server server;
 
+//String serverUrl = "http://localhost:3000"; // the remote server to upload to
+String serverUrl = "http://spectrometer.publiclaboratory.org"; // the remote server to upload to
 String controller = "setup"; // this determines what controller is used, i.e. what mode the app is in
 String colortype = "combined";
 final static String defaultTypedText = "type to label spectrum";
@@ -168,48 +172,3 @@ void draw() {
   updatePixels();
 }
 
-/**
- * POST pData to pUrl
- * @return the response
- */
-public String postData(URL pUrl, byte[] pData) {
-    // http://wiki.processing.org/w/Saving_files_to_a_web_server
-    try {
-        URLConnection c = pUrl.openConnection();
-        c.setDoOutput(true);
-        c.setDoInput(true);
-        c.setUseCaches(false);
-    
-        // set request headers
-        final String boundary = "AXi93A";
-        c.setRequestProperty("Content-Type", "multipart/form-data; boundary="+boundary);
- 
-        // open a stream which can write to the url
-        DataOutputStream dstream = new DataOutputStream(c.getOutputStream());
- 
-        // write content to the server, begin with the tag that says a content element is comming
-        dstream.writeBytes(boundary+"\r\n");
- 
-        // discribe the content
-        dstream.writeBytes("Content-Disposition: form-data; name=\"data\"; filename=\"whatever\" \r\nContent-Type: text/json\r\nContent-Transfer-Encoding: binary\r\n\r\n");
-        dstream.write(pData ,0, pData.length);
- 
-        // close the multipart form request
-        dstream.writeBytes("\r\n--"+boundary+"--\r\n\r\n");
-        dstream.flush();
-        dstream.close();
- 
-        // read the output from the URL
-        BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
-        StringBuilder sb = new StringBuilder(in.readLine());
-        String s = in.readLine();
-        while (s != null) {
-            s = in.readLine();
-            sb.append(s);
-        }
-        return sb.toString();
-    } catch (Exception e) {
-        e.printStackTrace();
-        return null;
-    }
-}
---
+++ spectrometry_kit/src/models/server.java
@@ -0,0 +1,73 @@
+class Server {
+  /**
+   * Save the current spectrum to the server
+   */
+  public void upload() {
+    //save CSV and JSON:
+    String spectraFolder = "spectra/";
+    SpectrumPresentation presenter = new SpectrumPresentation(spectrumbuf);
+
+    PrintWriter csv = createWriter(spectraFolder + presenter.generateFileName(typedText, "csv"));
+    csv.print(presenter.toCsv());
+    csv.close();
+
+    PrintWriter json = createWriter(spectraFolder + presenter.generateFileName(typedText, "json"));
+    json.print(presenter.toJson(presenter.generateFileName(typedText, null)));
+    json.close();
+
+    //save PNG:
+    save(spectraFolder + presenter.generateFileName(typedText, "png"));
+    //save to web:
+    println(serverUrl+"/spectrums/create?title="+typedText);
+    URL u = new URL("http://localhost:3000/spectrums/create?title="+typedText);
+    //URL u = new URL(serverUrl+"/spectrums/create?title="+typedText);
+    this.postData(u,presenter.toJson(presenter.generateFileName(typedText, null)).getBytes());
+    //clear label buffer
+    typedText = "";
+  }
+  /**
+   * POST pData to pUrl
+   * @return the response
+   */
+  public String postData(URL pUrl, byte[] pData) {
+    // http://wiki.processing.org/w/Saving_files_to_a_web_server
+    try {
+        URLConnection c = pUrl.openConnection();
+        c.setDoOutput(true);
+        c.setDoInput(true);
+        c.setUseCaches(false);
+    
+        // set request headers
+        final String boundary = "AXi93A";
+        c.setRequestProperty("Content-Type", "multipart/form-data; boundary="+boundary);
+ 
+        // open a stream which can write to the url
+        DataOutputStream dstream = new DataOutputStream(c.getOutputStream());
+ 
+        // write content to the server, begin with the tag that says a content element is comming
+        dstream.writeBytes(boundary+"\r\n");
+ 
+        // discribe the content
+        dstream.writeBytes("Content-Disposition: form-data; name=\"data\"; filename=\"whatever\" \r\nContent-Type: text/json\r\nContent-Transfer-Encoding: binary\r\n\r\n");
+        dstream.write(pData ,0, pData.length);
+ 
+        // close the multipart form request
+        dstream.writeBytes("\r\n--"+boundary+"--\r\n\r\n");
+        dstream.flush();
+        dstream.close();
+ 
+        // read the output from the URL
+        BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
+        StringBuilder sb = new StringBuilder(in.readLine());
+        String s = in.readLine();
+        while (s != null) {
+            s = in.readLine();
+            sb.append(s);
+        }
+        return sb.toString();
+    } catch (Exception e) {
+        e.printStackTrace();
+        return null;
+    }
+  }
+}
