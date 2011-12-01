51bf946f494e4710e3d4c6b7ba5d81f63f63a0a8
jywarren <jeff@unterbahn.com>
2011-12-01 08:46:13 -0500

    markdown fixes

--- webserver/app/helpers/application_helper.rb
+++ webserver/app/helpers/application_helper.rb
@@ -2,6 +2,6 @@
 module ApplicationHelper
   def markdown(text)
     options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
-    syntax_highlighter(Redcarpet.new(text, *options).to_html).html_safe
+    Redcarpet.new(text, *options).to_html
   end
 end
