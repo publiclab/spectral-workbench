da12385be595a165b076a259ebae9ad53b8bccd3
jywarren <jeff@unterbahn.com>
2011-12-01 01:10:40 -0500

    added flot library, starting graph code, markdown but not working, restart and stop quick scripts

--- webserver/app/helpers/application_helper.rb
+++ webserver/app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def markdown(text)
+    options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
+    syntax_highlighter(Redcarpet.new(text, *options).to_html).html_safe
+  end
 end
