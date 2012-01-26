77880ac96cf2dda4d8fc99dfaa713fca2b8279f9
Jeffrey Warren <jeff@unterbahn.com>
2012-01-26 11:03:10 -0500

    auto-generation of spectrum graphs from pixel data on server

--- webserver/app/models/spectrum.rb
+++ webserver/app/models/spectrum.rb
@@ -22,4 +22,30 @@ class Spectrum < ActiveRecord::Base
 	  :styles => {
 	    :thumb=> "300x100!",
 	    :large =>   "800x200!" }
+
+  # extracts serialized data from the top row of the stored image
+  def extract_data
+    require 'rubygems'
+    require 'RMagick'
+    pixels = []
+
+    image   = Magick::ImageList.new("public"+self.photo.url)
+    row = image.export_pixels(0, 1, image.columns, 1, "RGB");
+    (0..(row.length/3-1)).each do |p|
+      r = row[p*3]/255
+      g = row[p*3+1]/255
+      b = row[p*3+2]/255
+      pixels << '{wavelength:null,average:'+((r+g+b)/3).to_s+',r:'+r.to_s+',g:'+g.to_s+',b:'+b.to_s+'}'
+    end
+
+    puts pixels
+    # save it internally
+    s = "{name:'"+self.title+"',lines: ["
+    s += pixels.join(',')
+    s += "]}"
+    self.data = s
+    self.save
+    s
+  end
+
 end
