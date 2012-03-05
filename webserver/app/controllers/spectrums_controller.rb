4532aaabd56df09ec8697c9b61fe9d793f870636
Jeffrey Warren <jeff@unterbahn.com>
2012-03-05 14:11:51 -0500

    link to help

--- webserver/app/controllers/spectrums_controller.rb
+++ webserver/app/controllers/spectrums_controller.rb
@@ -90,7 +90,9 @@ class SpectrumsController < ApplicationController
   # POST /spectrums.xml
   # ?spectrum[title]=TITLE&spectrum[author]=anonymous&client=VERSION&uniq_id=UNIQID&startWavelength=STARTW&endWavelength=ENDW;
   def create
-    client_code = params[:client]+"::"+params[:uniq_id]
+    client = params[:client] || "0"
+    uniq_id = params[:uniq_id] || "0"
+    client_code = client+"::"+uniq_id
     puts client_code
 
     if params[:photo]
