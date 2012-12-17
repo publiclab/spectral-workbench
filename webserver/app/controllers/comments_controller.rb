49ecac2fe345ff2ca9bd99b2e2b990eaf390b5b0
jywarren <jeff@unterbahn.com>
2012-12-17 00:01:39 -0500

    comment deletion fix

--- webserver/app/controllers/comments_controller.rb
+++ webserver/app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
 
 	def delete
 		@comment = Comment.find(params[:id])
-		if @comment.author == current_user.login || params[:password].to_i == APP_CONFIG["password"]
+		if @comment.author == current_user.login || params[:password].to_i == APP_CONFIG["password"] || current_user.role == "admin"
 			@comment.destroy
 		end
 		redirect_to "/spectra/show/"+@comment.spectrum_id.to_s if params[:index] != "true"
