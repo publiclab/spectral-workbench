09079487ffbdede5e5176b6647992fc7e8ae3110
Jeffrey Warren <jeff@unterbahn.com>
2013-02-25 16:19:32 +0000

    fixed quotation marks in titles issue and missing graph bug; close #171

--- webserver/public/javascripts/capture.js
+++ webserver/public/javascripts/capture.js
@@ -67,7 +67,7 @@ $W = {
 		// retrieve and apply locally stored sample row height
 		if (localStorage.getItem('sw:sample_start_row')) this.sample_start_row = parseInt(localStorage.getItem('sw:sample_start_row'))
 		if (localStorage.getItem('sw:sample_end_row')) this.sample_end_row = parseInt(localStorage.getItem('sw:sample_end_row'))
-		this.sample_height = this.sample_end_row - this.sample_start_row // how many pixels to sample
+		this.sample_height = Math.abs(this.sample_end_row - this.sample_start_row) // how many pixels to sample
 		$W.setSampleRow($W.sample_start_row)
 
 	},
