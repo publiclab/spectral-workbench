43eaf8bb38484c1f983634086df2e1cd1eed9fbb
Jeffrey Warren <jeff@unterbahn.com>
2013-06-07 14:02:50 +0000

    comment moderation by set/spectrum authors

--- webserver/app/models/comment.rb
+++ webserver/app/models/comment.rb
@@ -1,21 +1,25 @@
 class Comment < ActiveRecord::Base
 
-	validates_presence_of :author, :email, :body
+  validates_presence_of :author, :email, :body
 
-	def set
-		if self.spectra_set_id == 0
-			false
-		else
-			Set.find self.spectra_set_id
-		end
-	end
+  def set
+    if self.spectra_set_id == 0
+      false
+    else
+      Set.find self.spectra_set_id
+    end
+  end
 
-	def spectrum
-		if self.spectrum_id == 0 || self.spectrum_id.nil?
-			false
-		else
-			Spectrum.find self.spectrum_id
-		end
-	end
+  def spectrum
+    if self.spectrum_id == 0 || self.spectrum_id.nil?
+      false
+    else
+      Spectrum.find self.spectrum_id
+    end
+  end
+
+  def can_delete(user)
+    (self.set && self.set.author == user.login) || (self.spectrum && self.spectrum.author == user.login) || self.author == user.login || user.role == "admin"
+  end
 
 end
