08df09bc499916b4dad962f35e04e4cf4fc3253d
jywarren <jeff@unterbahn.com>
2013-06-26 23:10:28 -0400

    correct openid server

--- webserver/app/controllers/sessions_controller.rb
+++ webserver/app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    params[:openid_url] = "localhost:3000/openid/"+params[:login] if params[:login]
-    #params[:openid_url] = "publiclab.org/openid/"+params[:login] if params[:login]
+    #params[:openid_url] = "localhost:3000/openid/"+params[:login] if params[:login]
+    params[:openid_url] = "publiclab.org/openid/"+params[:login] if params[:login]
     #params[:openid_url] = "localhost/people/"+params[:login]+"/identity" if params[:login]
     #params[:openid_url] = "publiclaboratory.org/people/"+params[:login]+"/identity" if params[:login]
     #params[:openid_url] = "publiclab.org/openid/"+params[:openid][:identity] if params[:openid] && params[:openid][:identity]
