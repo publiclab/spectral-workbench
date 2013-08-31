79769f0b9bc5bd543f98c3e4a3a9475d9dbb73a5
Sreyantha Chary <sreyanth@gmail.com>
2013-08-31 21:31:40 +0530

    Added controller functions for matching

--- webserver/app/controllers/match_controller.rb
+++ webserver/app/controllers/match_controller.rb
@@ -3,20 +3,71 @@ class MatchController < ApplicationController
   def index
     id = params[:id]
     
-    if id.nil?
-      @d = "Welcome"
+    if id.nil? # No need to search anything
+      @message = "Welcome for the searching interface"
     else
-      @d = search(id)
+      @spectrum = ProcessedSpectrum.find_by_spectrum_id(id)
+      if @spectrum # Found a soectrum with specified id.
+        
+        if params[:c] # Flag to check if the rendering is for comparing or displaying
+	      @c = 1
+        end
+        
+        @spectra = @spectrum.closest_match(params[:fit].to_i)
+        render :partial => "match_results", :layout => false
+        
+        return # Cannot render two times. So return
+      
+      else # Didnt find a spectrum with specified id
+        @message = "Not found"
+      
+      end
     end
 
     render :layout => "bootstrap"
   end
 
 
-  def search(id)
-    ## Search for the candidates here
+  def search
+    id = params[:id]
+    range = params[:fit]
     
-    return "Results will be here"
+    if range.nil? || range == "" || (range != '0' && range.to_i == 0)
+      range = 100
+    end
+    
+    @range = range.to_i
+    
+    if @range < 0 || @range > 150
+      @range = 100
+      @err = "The fit input must be between 0 and 150"
+    end
+    
+    @spectrum = Spectrum.find(id)
+    
+    if @spectrum.data == "" || @spectrum.data.nil?
+      @spectrum.extract_data 
+      @spectrum.save 
+    end
+    
+    @spectra = ProcessedSpectrum.find_by_spectrum_id(id).closest_match(@range)
+    @sets = @spectrum.sets
+    @macros = Macro.find :all, :conditions => {:macro_type => "analyze"}
+    @calibrations = current_user.calibrations if logged_in?
+    @comment = Comment.new
+    
+    respond_to do |format|
+      format.html { render :layout => "bootstrap" }
+      format.xml  { render :xml => @spectrum }
+      format.csv  { 
+        if params[:raw]
+          render :template => "spectrums/raw.csv.erb" 
+        else
+          render :template => "spectrums/show.csv.erb" # formatted for SpectraOnline.com 
+        end
+      }
+      format.json  { render :json => @spectrum }
+    end
   end
 
 end
