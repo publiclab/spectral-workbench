2dbf78f06d1825f43be39b15d9b97d906aa7819c
Sreyantha Chary <sreyanth@gmail.com>
2013-09-10 15:51:54 +0530

    Enhanced the match controller. Final Commit

--- webserver/app/controllers/match_controller.rb
+++ webserver/app/controllers/match_controller.rb
@@ -4,23 +4,29 @@ class MatchController < ApplicationController
     id = params[:id]
     
     if id.nil? # No need to search anything
-      @message = "Welcome for the searching interface"
+      flash[:error] = "ID is required for matching"
+      redirect_to "/dashboard/"
+      return
+      
     else
-      @spectrum = ProcessedSpectrum.find_by_spectrum_id(id)
-      if @spectrum # Found a soectrum with specified id.
+      @proc_spectrum = ProcessedSpectrum.find_by_spectrum_id(id)
+      
+      if @proc_spectrum # Found a spectrum with specified id.
+        @spectra = @proc_spectrum.closest_match(params[:fit].to_i, 20)
+        @spectrum = Spectrum.find(id)
         
         if params[:c] # Flag to check if the rendering is for comparing or displaying
-	      @c = 1
+	      render :partial => "match_results", :layout => false
+	      return
+	    else
+	      render :partial => "list_results", :layout => false
+	      return
         end
-        
-        @spectra = @spectrum.closest_match(params[:fit].to_i,20)
-        render :partial => "match_results", :layout => false
-        
-        return # Cannot render two times. So return
       
       else # Didnt find a spectrum with specified id
-        @message = "Not found"
-      
+        flash[:error] = "A spectrum with the specified ID is not found"
+        redirect_to "/dashboard/"
+        return
       end
     end
 
@@ -49,12 +55,45 @@ class MatchController < ApplicationController
       @spectrum.extract_data 
       @spectrum.save 
     end
+
+    if !@spectrum.calibrated
+      flash[:error] = "The spectrum is not calibrated. If you are the author of this spectrum please calibrate it first"
+      redirect_to "/analyze/spectrum/" + id.to_s
+      return
+    end
+    
+    proc_nil = false
     
     @processed_spectra = ProcessedSpectrum.find_by_spectrum_id(id)
     if @processed_spectra.nil? || @processed_spectra == ""
-      @spectra = @spectrum
+      @spectra = [@spectrum]
+      proc_nil = true
     else
-      @spectra = @processed_spectra.closest_match(@range,20)
+      @spectra = @processed_spectra.closest_match(@range, 20)
+    end
+    
+    # Some spectrums have many matches with range of 100. Some have very few.
+    # So why stop just at 100? Something dynamic would be good, though takes some extra time
+    # Implementing a sort of binary search for best spectra matching.
+    
+    # Time to make our matches more meaningful.
+
+    range_visits = [@range] # To check the ranges visited
+
+    # This loop will take 5 iterations at maximum.
+    while !proc_nil and (@spectra.size < 2 or @spectra.size > 6)
+      if @spectra.size > 6 # Need to reduce the range
+       	@range = @range - 10
+      else
+	@range = @range + 10
+      end
+      
+      if range_visits.member?(@range) or @range < 10 or @range > 150
+        break
+      end
+      
+      range_visits.push(@range)
+      @spectra = @processed_spectra.closest_match(@range, 20)
     end
     
     @sets = @spectrum.sets
