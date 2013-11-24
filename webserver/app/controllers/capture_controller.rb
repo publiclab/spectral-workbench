e1daf3c5d7bf98f8324d40d7ca9e5cbf0b5fe831
Jeffrey Warren <jeff@unterbahn.com>
2013-11-24 21:46:53 +0000

    dont cache while offline

--- webserver/app/controllers/capture_controller.rb
+++ webserver/app/controllers/capture_controller.rb
@@ -7,8 +7,8 @@ class CaptureController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def index
-    @offline = true
     if logged_in?
+      @offline = true
       @calibration = current_user.last_calibration
       @calibration = Spectrum.find(params[:calibration_id]) if params[:calibration_id]
       @start_wavelength,@end_wavelength = @calibration.wavelength_range if @calibration
