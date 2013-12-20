4bff003a230c3d5aaf5be482fbeb8cea422db667
jywarren <jeff@unterbahn.com>
2013-12-20 16:40:07 -0500

    better capture defaults and saying nice things to IE users

--- webserver/app/helpers/application_helper.rb
+++ webserver/app/helpers/application_helper.rb
@@ -23,6 +23,10 @@ module ApplicationHelper
     request.env['HTTP_USER_AGENT'] && request.env['HTTP_USER_AGENT'].match("Opera")
   end
 
+  def webrtc?
+    request.env['HTTP_USER_AGENT'] && (request.env['HTTP_USER_AGENT'].match("Opera") || request.env['HTTP_USER_AGENT'].match("Firefox") || request.env['HTTP_USER_AGENT'].match("Chrome"))
+  end
+
   def mobile?
     request.env['HTTP_USER_AGENT'] && request.env['HTTP_USER_AGENT'].match("Mobi")
   end
