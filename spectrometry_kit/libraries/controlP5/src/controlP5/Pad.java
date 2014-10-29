6dc28e600a10989f5fbca25414a26ae05947245c
jywarren <jeff@unterbahn.com>
2014-10-29 14:15:10 -0400

    root directory cleanup!!! yay

--- spectrometry_kit/libraries/controlP5/src/controlP5/Pad.java
+++
@@ -1,6 +0,0 @@
-package controlP5;
-
-public class Pad {
-	// pad for placing controllers, like matrix but without the grid
-	// a sequencer
-}
--- spectrometry_kit/libraries/controlP5/src/controlP5/Tooltip.java
+++
@@ -1,5 +0,0 @@
-package controlP5;
-
-class Tooltip {
-	// TODO a tooltip for controllers. on enter, on leave.
-}
