7c8427a6fe970718a9c4722f36350a1e1c6e949e
Viduranga Wijesooriya <vpowerrc@gmail.com>
2014-12-23 01:52:02 +0530

    added javascript file for dashboard page
    Signed-off-by: Viduranga Wijesooriya <vpowerrc@gmail.com>

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,25 +1,4 @@
-jQuery(document).ready(function() {
-  $('#searchform').submit(function(e){
-    e.preventDefault()
-    window.location = '/search/'+$('#searchform_input').val()
-  })
-  // working off of http://stackoverflow.com/questions/9232748/twitter-bootstrap-typeahead-ajax-example
-  $('#searchform_input').typeahead({
-    source: function (typeahead, query) {
-      return $.post('/search/typeahead/'+query, {}, function (data) {
-        return typeahead.process(data)
-      })
-    },
-    items: 15,
-    // Autoselect is disabled so that users can enter new tags
-    autoselect: false,
-    autowidth: false
-  })
-
-  var url = document.location.toString();
-  if (url.match('#')) {
-    $('#main_tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
-  }
-
-
-})
+var url = document.location.toString();
+if (url.match('#')) {
+  $('#main_tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
+}
