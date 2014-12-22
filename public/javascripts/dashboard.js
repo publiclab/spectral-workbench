5708ea6cbaf5c1891d9afd506e445d4637ef807b
Viduranga Wijesooriya <vpowerrc@gmail.com>
2014-12-23 01:56:34 +0530

    added javascript file for dashboard page
    Signed-off-by: Viduranga Wijesooriya <vpowerrc@gmail.com>

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,4 +1,6 @@
-var url = document.location.toString();
-if (url.match('#')) {
-  $('#main_tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
-}
+jQuery(document).ready(function() {
+  var url = document.location.toString();
+  if (url.match('#')) {
+    $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
+  }
+})
