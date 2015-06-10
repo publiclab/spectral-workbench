41f86cdac0f910f90b5c06c6df960270f7241741
jywarren <jeff@unterbahn.com>
2015-06-10 13:36:35 -0400

    macro fixes

--- app/models/macro.rb
+++ app/models/macro.rb
@@ -10,4 +10,6 @@ class Macro < ActiveRecord::Base
                             :on => :create                  
   validates_format_of :url, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
 
+  attr_accessible :title, :description, :code, :macro_type, :url, :user_id
+
 end
