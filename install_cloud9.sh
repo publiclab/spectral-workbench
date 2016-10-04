1697f78fe22a180451fa670b43f17abf9ebc4b62
Eric Watson <wasnotrice@gmail.com>
2016-10-04 03:40:57 +0000

    Update cloud9 install script
    The script failed to install imagemagick and friends, but still exited
    successfully and told me the application was running. My install was also
    missing bower, which the install script assumes is already installed. This
    commit fixes the install and makes sure that, if any of the install steps
    fail, the whole script fails, so that users understand that an error
    occurred.

--- install_cloud9.sh
+++ install_cloud9.sh
@@ -1,8 +1,14 @@
+#!/bin/bash
+set -e -o pipefail
+
 read -p "Enter your cloud9 username: " un
 rvm install ruby-2.1.2
 rvm 2.1.2
+sudo apt-get update
 sudo apt-get install libmagickwand-dev libmagick++-dev imagemagick ruby-rmagick
+sudo apt-get install nodejs npm
 mysql-ctl start
+npm install bower
 bower install
 bundle install
 rake cloud9 username=$un
