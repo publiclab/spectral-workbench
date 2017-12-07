3baf3d45bc6920724cd8336db267f59810b162ae
jywarren <jeff@unterbahn.com>
2017-12-07 17:38:33 -0500

    fix for test

--- test/unit/snapshot_test.rb
+++ test/unit/snapshot_test.rb
@@ -395,7 +395,7 @@ class SnapshotTest < ActiveSupport::TestCase
     3200.times do
       snapshot.data += '{"r":10,"g":10,"b":10,"average":10,"wavelength":400},'
     end
-    snapshot.data = '{"r":10,"g":10,"b":10,"average":10,"wavelength":400}]}'
+    snapshot.data += '{"r":10,"g":10,"b":10,"average":10,"wavelength":400}]}'
 
     snapshot.save!
 
