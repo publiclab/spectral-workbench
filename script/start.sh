09c9d5e691a4976ae6217c90dca1738deb43fca2
Alax Alves <alaxallves@gmail.com>
2019-12-23 14:37:40 -0300

    Containerizing MySQL and improving development env setup

---
+++ script/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+pidfile=/app/tmp/pids/server.pid
+
+bower install --allow-root
+cp config/database.yml.example config/database.yml
+cp config/config.yml.example config/config.yml
+
+until nc -z -v -w30 db 3306; do
+ echo 'Waiting for MySQL...'
+ sleep 1
+done
+
+echo "MySQL is up and running!"
+
+bundle exec rake db:setup
+bundle exec rake db:migrate
+
+if [ -f $pidfile ] ; then
+	>&2 echo 'Server PID file already exists. Removing it...';
+	rm $pidfile;
+fi
+
+bundle exec rails s -p 5000 -b '0.0.0.0'
