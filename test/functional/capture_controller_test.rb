8f6e0b42cb8ba8c287386c160be94edfc15e3f19
gr455 <ruturaj.mohite@iiitb.org>
2020-06-26 21:35:36 +0530

    More tests and syntax changes

--- test/functional/capture_controller_test.rb
+++ test/functional/capture_controller_test.rb
@@ -35,4 +35,32 @@ class CaptureControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should load the offline page" do
+    session[:user_id] = users(:quentin).id
+    get :offline
+
+    assert_response :success
+  end
+
+  test "should save a spectrum" do
+    session[:user_id] = users(:quentin).id
+    post :save,
+    title: "A very cool spectrum",
+    notes: "One of the best ones I have had so far",
+    img: "data:image/gif;base64,R0lGODdhMAAwAPAAAAAAAP///ywAAAAAMAAwAAAC8IyPqcvt3wCcDkiLc7C0qwyGHhSWpjQu5yqmCYsapyuvUUlvONmOZtfzgFzByTB10QgxOR0TqBQejhRNzOfkVJ+5YiUqrXF5Y5lKh/DeuNcP5yLWGsEbtLiOSpa/TPg7JpJHxyendzWTBfX0cxOnKPjgBzi4diinWGdkF8kjdfnycQZXZeYGejmJlZeGl9i2icVqaNVailT6F5iJ90m6mvuTS4OK05M0vDk0Q4XUtwvKOzrcd3iq9uisF81M1OIcR7lEewwcLp7tuNNkM3uNna3F2JQFo97Vriy/Xl4/f1cf5VWzXyym7PHhhx4dbgYKAAA7",
+    data: '{"lines":[{"r":10,"g":10,"b":10,"average":10,"wavelength":400},{"r":10,"g":10,"b":10,"average":10,"wavelength":700}]}'
+
+    assert_response :redirect
+    assert_equal "Spectrum was successfully created.", flash[:notice]
+  end
+
+  test "should get recent calibrations" do
+    session[:user_id] = users(:quentin).id
+    get :recent_calibrations,
+    format: "json"
+
+    assert_response :success
+
+  end
+
 end
