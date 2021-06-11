22538705410544f27994383d8f84f7cbff1894de
Jeffrey Warren <jeff@unterbahn.com>
2021-06-11 14:02:55 -0400

    only use bootsnap if ENV['RAILS_ENV'] == 'production'

--- config/boot.rb
+++ config/boot.rb
@@ -3,4 +3,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+if ENV['RAILS_ENV'] == 'production' require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
