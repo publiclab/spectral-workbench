b1d0cacaa2c10ab0194a3d97a294592169fae8c5
waridrox <mohdwarid4@gmail.com>
2021-08-20 00:16:15 +0530

    Added tests for new capture page

--- test/system/newcapture_test.rb
+++ test/system/newcapture_test.rb
@@ -1,36 +1,101 @@
 require "application_system_test_case"
 
 class NewcaptureTest < ApplicationSystemTestCase
+  Capybara.default_max_wait_time = 60
 
-  test 'visiting the capture page' do 
-    visit '/capture'
+  def setup
+    @user = User.create({
+        login: Faker::Internet.username,
+        name: Faker::Name.name,
+        email: Faker::Internet.email 
+    })
   end
 
-  test 'visiting the new capture page' do
-    visit '/capture/v2'
-    
-    # 1 How to enable the in browser prompt for accessing the device camera 
-    # 2 How to simulate login of the user (since we can only log in the app with other social media accounts)
+  test 'new capture spectrum page' do
+    #log in 
+    page.set_rack_session(:user_id => @user.id)
+    visit "/capture/v2"
 
-    # dismissing the login model
-    assert_selector('h4.modal-title', text: 'Log in for full functionality')
-    find("div.modal-footer #login-dismiss").click()
+    # Enable the in browser prompt for accessing the device camera (use-fake-ui-for-media-stream (Not working))
 
-    # if the user isn't logged in, then a `Log in` button should be displayed in the navbar
-    loginButton = page.find('#login-btn')
-    assert_equal(loginButton.text, 'Log in')
+    loginStatus = page.find('p#login-status')
+    assert_equal(loginStatus.text, "(logged in as #{@user.login})")
 
     # going to the settings page
     find("button#landing-page-next").click()
-    find("a.btn-rotate").click() # checking for vertical spectrum
-    find("a.btn-rotate").click() # back to horizontal spectrum
+    assert_selector('video#webcam-video')
+
+    # Testing buttons
+    # horizontal spectrum image is shown by default
+    assert_selector('img.img-rot')
+    # vertical spectrum image is hidden
+    find('img#cfl-resp', visible: false)
+
+    find("a.btn-rotate").click() # rotate 90 degrees
+    
+    # vertical spectrum image is shown
+    assert_selector('img#cfl-resp')
+    # horizontal spectrum image is hidden
+    find('img.img-rot', visible: false)
+
+    find("a.btn-rotate").click() # rotate 90 degrees (back to horizontal spectrum)
+
+    find("video#webcam-video").assert_matches_style("transform" => "none")
+
+    # clicking the flip-image button
+    find("a.btn-flip").click()
+    find("video#webcam-video").assert_matches_style("transform" => "matrix(-1, 0, 0, 1, 0, 0)")
+
     find("button#setting-page-next").click()
 
     accept_alert do 
       find("button#json-download").click()
     end
 
-  #checking the final view of the test
-  sleep(2)
+    # goto save spectrum page
+    find("button#capture-page-next").click()
+    
+    # preview of the spectrum should be displayed
+    assert_selector('img#spectrum-preview')      
+    
+    # login status is displayed on the save page
+    assert_selector('i', text: "(logged in as #{@user.login})")
+
+    # filling in the save form with data
+    save_form_title = page.find('input#saveFormTitle')
+    save_form_title
+      .click
+      .fill_in with: "Title of the test spectrum"
+
+    save_form_tags = page.find('input#spectrum_tags')
+    save_form_tags
+      .click
+      .fill_in with: "test, spectrum, rgb"
+
+    save_form_notes = page.find('textarea#spectrum_notes')
+    save_form_notes
+      .click
+      .fill_in with: "This is the notes section of the test spectrum."
+
+    # saving with selected calibration
+    new_window = window_opened_by { click_link 'Save with selected calibration' }
+
+    # following the page on new spectrum creation (/spectrums/{spectrum.id})
+    within_window new_window do
+      # check if the spectrum was successfully created
+      page.find('div.alert-success', text: "Spectrum was successfully created.")
+
+      assert_selector('a', text: 'Title of the test spectrum')
+      assert_selector('div.span4 p', text: 'This is the notes section of the test spectrum.')
+
+      page.find('p.credit a', text: "#{@user.login}")
+
+      accept_alert do 
+        find("div.span4 div.btn-group i.fa-trash").click()
+      end
+      
+      page.find('div.alert-success', text: "Spectrum deleted.")
+    end
   end
+
 end
