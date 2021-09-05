9a4e403deb049e0717e8c2d4b02db75606d15b94
Jeffrey Warren <jeff@unterbahn.com>
2021-09-05 15:32:50 -0400

    Update spectrums_controller_test.rb

--- test/controllers/spectrums_controller_test.rb
+++ test/controllers/spectrums_controller_test.rb
@@ -250,7 +250,7 @@ class SpectrumsControllerTest < ActionController::TestCase
     assert_response :redirect
     # to check if the new spectrum title was updated
     assert_equal 'Spectrum was successfully updated.', flash[:notice]
-    assert_equal "Really cool spectrum" , Spectrum.find(spectrums(:one)).title
+    assert_equal "Really cool spectrum" , Spectrum.find(spectrums(:one).id).title
     assert_not_nil :spectrum
     assert_redirected_to spectrum_path(assigns(:spectrum))
   end
@@ -262,7 +262,7 @@ class SpectrumsControllerTest < ActionController::TestCase
     assert_response :redirect
     # to check if the new spectrum notes was updated
     assert_equal 'Spectrum was successfully updated.', flash[:notice]
-    assert_equal "Neon spectrum info", Spectrum.find(spectrums(:one)).notes
+    assert_equal "Neon spectrum info", Spectrum.find(spectrums(:one).id).notes
     assert_not_nil :spectrum
     assert_redirected_to spectrum_path(assigns(:spectrum))
   end
