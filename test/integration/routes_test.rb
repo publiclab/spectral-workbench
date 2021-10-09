6b9110a5f1ac4bce0ab63892bd2458908d8f7d96
Paulina Gadea <89214652+PaulinaGadea@users.noreply.github.com>
2021-10-09 17:14:03 -0300

    Update routes_test.rb

--- test/integration/routes_test.rb
+++ test/integration/routes_test.rb
@@ -5,7 +5,12 @@ require 'test_helper'
 class RoutesTest < ActionDispatch::IntegrationTest
   fixtures :all
 
-  test "test spectrums choose route" do
+   "test spectrums choose route" do
     assert_routing({ path: '/spectrums/choose', method: :post }, { controller: 'spectrums', action: 'choose' })
   end
+  
+   test "test get request for sets embed2" do
+     assert_routing({ path: '/sets/embed2/:id', method: 'get' }, {controller: 'sets', action: 'embed2', id: ':id' })
+  end
+
 end
