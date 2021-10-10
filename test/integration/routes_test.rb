9ab46c7b2e712363ec8118964771972c8b92bf14
Vaithee <58522375+VaithiSniper@users.noreply.github.com>
2021-10-10 20:18:13 +0530

    changed indentation

--- test/integration/routes_test.rb
+++ test/integration/routes_test.rb
@@ -4,9 +4,9 @@ require 'test_helper'
 
 class RoutesTest < ActionDispatch::IntegrationTest
   fixtures :all
- test "test get request for sets create" do
- assert_routing({ path: '/sets/create/:id', method: 'post' }, {controller: 'sets', action: 'create', id: ':id' })
- end
+  test "test get request for sets create" do
+     assert_routing({ path: '/sets/create/:id', method: 'post' }, {controller: 'sets', action: 'create', id: ':id' })
+  end
 
   test "test spectrums choose route" do
     assert_routing({ path: '/spectrums/choose', method: :post }, { controller: 'spectrums', action: 'choose' })
