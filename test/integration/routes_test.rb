e29f2f59e8da695a30469ba00500ab175770e6a9
Sarah Loutrel <71740607+sloutrel@users.noreply.github.com>
2021-10-09 21:04:01 -0700

    Add new test to routes_test.rb

--- test/integration/routes_test.rb
+++ test/integration/routes_test.rb
@@ -4,6 +4,10 @@ require 'test_helper'
 
 class RoutesTest < ActionDispatch::IntegrationTest
   fixtures :all
+  
+  test "test get request for sets embed" do
+    assert_routing({ path: '/sets/embed/:id', method: 'get' }, {controller: 'sets', action: 'embed', id: ':id' })
+  end
 
   test "test spectrums choose route" do
     assert_routing({ path: '/spectrums/choose', method: :post }, { controller: 'spectrums', action: 'choose' })
