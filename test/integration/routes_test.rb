1b4ff0a333c076d8216bab83ef5de3bd6e476adb
Sarah Loutrel <71740607+sloutrel@users.noreply.github.com>
2021-10-19 00:22:14 -0700

    Add sets embed test to routes_test.rb (#747)
    Co-authored-by: Sneha Mishra <33183263+Tlazypanda@users.noreply.github.com>

--- test/integration/routes_test.rb
+++ test/integration/routes_test.rb
@@ -4,6 +4,11 @@ require 'test_helper'
 
 class RoutesTest < ActionDispatch::IntegrationTest
   fixtures :all
+  
+  test "test get request for sets embed" do
+    assert_routing({ path: '/sets/embed/:id', method: 'get' }, {controller: 'sets', action: 'embed', id: ':id' })
+  end
+
   test "test get request for sets create" do
     assert_routing({ path: '/sets/create/:id', method: 'post' }, {controller: 'sets', action: 'create', id: ':id' })
   end
