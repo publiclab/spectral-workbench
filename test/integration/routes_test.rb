1af2b2733601e178016ff6b533340bb3683d718d
vikramlance <vikram.lance@gmail.com>
2021-10-20 20:28:50 -0500

    Added routes test for spectrums/embed2/:id

--- test/integration/routes_test.rb
+++ test/integration/routes_test.rb
@@ -8,6 +8,10 @@ class RoutesTest < ActionDispatch::IntegrationTest
   test "test get request for sets embed" do
     assert_routing({ path: '/sets/embed/:id', method: 'get' }, {controller: 'sets', action: 'embed', id: ':id' })
   end
+  
+  test "test get request for spectrums embed2" do
+    assert_routing({ path: '/spectrums/embed2/:id', method: 'get' }, {controller: 'spectrums', action: 'embed2', id: ':id' })
+  end  
 
   test "test get request for sets create" do
     assert_routing({ path: '/sets/create/:id', method: 'post' }, {controller: 'sets', action: 'create', id: ':id' })
