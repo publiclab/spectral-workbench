8bbbe257a5426cbdc7c9171ee08b55b91a72d1db
Rhona Lam <81948172+rhonall@users.noreply.github.com>
2021-12-15 17:15:27 +1000

    Update routes_test.rb (#799)
    Added routes test for sets/embed2/:id

--- test/integration/routes_test.rb
+++ test/integration/routes_test.rb
@@ -16,6 +16,10 @@ class RoutesTest < ActionDispatch::IntegrationTest
   test "test get request for sets create" do
     assert_routing({ path: '/sets/create/:id', method: 'post' }, {controller: 'sets', action: 'create', id: ':id' })
   end
+  
+  test "test get request for sets embed2" do
+    assert_routing({ path: '/sets/embed2/:id', method: 'get' }, {controller: 'sets', action: 'embed2', id: ':id' })
+  end
 
   test "test spectrums choose route" do
     assert_routing({ path: '/spectrums/choose', method: :post }, { controller: 'spectrums', action: 'choose' })
