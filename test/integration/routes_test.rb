ae516a20b0e55a277725a71fe72bd5c7d6d18e55
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2022-01-07 18:06:32 +0530

    chore: added routes test for 'match/search' (#827)
    * chore: added routes test for 'match/search'
    
    * fix: changed the match search test
    
    Co-authored-by: Sneha Mishra <33183263+Tlazypanda@users.noreply.github.com>

--- test/integration/routes_test.rb
+++ test/integration/routes_test.rb
@@ -47,17 +47,24 @@ class RoutesTest < ActionDispatch::IntegrationTest
 
   test "test post request for match search id" do
     assert_routing({ path: '/match/search/:id', method: 'post' }, {controller: 'match', action: 'search', id: ':id' })
-
   end
+  
   test "test get request for match id" do
     assert_routing({ path: '/match/:id', method: 'get' }, {controller: 'match', action: 'index', id: ':id' })
   end
+  
+  test "test get request for match search" do
+	  assert_routing({ path: '/match/search', method: 'get' }, {controller: 'match', action: 'index', id: 'search' })
+  end
+  
   test "test get request for match livesearch" do
     assert_routing({ path: '/match/livesearch', method: 'get' }, {controller: 'match', action: 'livesearch' })
   end
+
   test "test get request for match get_conditions" do
     assert_routing({ path: '/match/get_conditions', method: 'get' }, {controller: 'match', action: 'index', id: 'get_conditions' })
   end
+
   test "test post request for spectrums choose" do
     assert_routing({ path: '/spectrums/choose/:id', method: 'post' }, {controller: 'spectrums', action: 'choose', id: ':id' })
   end
