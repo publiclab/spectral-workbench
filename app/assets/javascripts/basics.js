fd4f06d04a9ed1a2b268c4b40ffc9efae2e32f5c
Jeffrey Warren <jeff@unterbahn.com>
2022-05-19 15:04:37 -0400

    Update basics.js

--- app/assets/javascripts/basics.js
+++ app/assets/javascripts/basics.js
@@ -63,17 +63,6 @@ window.addEventListener('load', function(e) {
   }
 }, false);
 
-/* Google Analytics */
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-30020531-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
 /* Facebook button crap */
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
